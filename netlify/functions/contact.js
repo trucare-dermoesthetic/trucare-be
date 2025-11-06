5fab32ec845f8f80dd8e5cd18dea2997cb5142a2
OscarTrujillo <oscartrujillo94@gmail.com>
2025-11-06 16:26:36 +0100

    feat: new functions

---
+++ netlify/functions/contact.js
@@ -0,0 +1,76 @@
+export async function handler(event) {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin":
+          "https://trucare-dermoesthetic.github.io",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+      },
+      body: "",
+    };
+  }
+
+  console.log("Event Body:", JSON.parse(event.body));
+
+  const { name, email, phone, message } = JSON.parse(event.body);
+
+  console.log("BREVO_API_KEY exists:", !!process.env.BREVO_API_KEY);
+
+  const sendAt = new Date();
+
+
+  // sendEmail to TruCare
+  const resTrucare = await fetch("https://api.brevo.com/v3/smtp/email", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "api-key": process.env.BREVO_API_KEY,
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      sender: { name: "TruCare", email: "trucare.carla@gmail.com" },
+      to: [{ email: "trucare.carla@gmail.com" }],
+      subject: "Nuevo mensaje desde el formulario de contacto 💛",
+      textContent: `Has recibido un nuevo mensaje a través de la web:
+        Nombre: ${name}
+        Email: ${email}
+        Teléfono: ${phone}
+        Mensaje: ${message}
+        Fecha: ${sendAt.toLocaleString()}
+        `,
+    }),
+  });
+
+  // sendEmail to user
+  await fetch("https://api.brevo.com/v3/smtp/email", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "api-key": process.env.BREVO_API_KEY,
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      sender: { name: "TruCare", email: "trucare.carla@gmail.com" },
+      to: [{ email: email }],
+      subject: "Hemos recibido tu mensaje 💛",
+      textContent: ` Hola ${name},
+
+ Gracias por ponerte en contacto conmigo. He recibido tu mensaje y te responderé lo antes posible.
+ Mientras tanto, gracias por confiar en TruCare.
+ Estoy aquí para ayudarte en lo que necesites.
+
+ Un abrazo,
+ TruCare
+        `,
+    }),
+  });
+
+  // const data = await resTrucare.json();
+  // JSON.stringify(data)
+  return {
+    statusCode: resTrucare.ok ? 200 : 400,
+    body: "ok",
+  };
+}
---
+++ netlify/functions/registerNewsletter.js
@@ -0,0 +1,62 @@
+import { db } from "./firebaseAdmin";
+
+export async function handler(event) {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin":
+          "https://trucare-dermoesthetic.github.io",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+      },
+      body: "",
+    };
+  }
+
+  console.log("Event Body:", JSON.parse(event.body));
+
+  const { name, email, phone } = JSON.parse(event.body);
+
+  console.log("BREVO_API_KEY exists:", !!process.env.BREVO_API_KEY);
+
+  const sendAt = new Date();
+
+  await db.collection("registerNewsletter").add({
+    name,
+    email,
+    phone,
+    sentAt: sendAt,
+  });
+
+  // sendEmail to user
+  await fetch("https://api.brevo.com/v3/smtp/email", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "api-key": process.env.BREVO_API_KEY,
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      sender: { name: "TruCare", email: "trucare.carla@gmail.com" },
+      to: [{ email: email }],
+      subject: "¡Bienvenida/o a TruCare! 💛",
+      textContent: `Hola ${name},
+
+          Gracias por unirte a nuestra lista de promociones. A partir de ahora recibirás ofertas exclusivas, ventajas especiales y novedades pensadas para cuidar tu piel y tu bienestar.
+
+          Estoy encantada de acompañarte en este camino.
+
+          Un abrazo,
+        TruCare
+        `,
+    }),
+  });
+
+  // const data = await resTrucare.json();
+  // JSON.stringify(data)
+  return {
+    statusCode: resTrucare.ok ? 200 : 400,
+    body: "ok",
+  };
+}
